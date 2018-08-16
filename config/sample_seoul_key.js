e7984af1f040785cbe57545d4a8652ce791be042
wkdalstn84 <didgustn319@naver.com>
2018-08-16 13:52:56 +0900

    first

---
+++ config/sample_seoul_key.js
@@ -0,0 +1,8 @@
+var seoul_api = {
+		api_key : '6a774c754f776b64313235536b555047',
+		api_url : 'http://openAPI.seoul.go.kr:8088/',
+		api_name : 'SearchPerformanceBySubjectService'
+		
+}
+
+exports.seoul = seoul_api
